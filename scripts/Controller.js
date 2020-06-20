8ecac1dee45c6c33016920117c4bffdfd3f7d88d
aleksergie <aleksergie@ukr.net>
2020-06-20 14:51:42 +0300

    Changed methods in Controller to arrow functions

--- scripts/Controller.js
+++ scripts/Controller.js
@@ -2,42 +2,41 @@ class Controller {
   constructor(model, view) {
     this.model = model;
     this.view = view;
-    this.model.bindListChanged(this.onPersonListChanged.bind(this));
+    this.model.bindListChanged(this.onPersonListChanged);
     this.view.bindIncrementLikes(this.handleIncrementLikes);
-    // this.view.bindIncrementLikes(this.handleIncrementLikes.bind(this));
-    this.view.bindShowModal(this.handleModal.bind(this));
-    this.view.bindChangeoverNext(this.handleChangeoverNext.bind(this));
-    this.view.bindChangeoverPrev(this.handleChangeoverPrev.bind(this));
+    this.view.bindShowModal(this.handleModal);
+    this.view.bindChangeoverNext(this.handleChangeoverNext);
+    this.view.bindChangeoverPrev(this.handleChangeoverPrev);
     this.onPersonListChanged(this.model.persons, null);
   }
 
-  onPersonListChanged(persons, id) {
+  onPersonListChanged = (persons, id) => {
     this.view.render(persons);
     let person = this.model.getPersonForModal(id);
     this.view.renderModal(person);
-  }
+  };
 
   handleIncrementLikes = (id) => {
     this.model.editLikes(id);
   };
 
-  handleModal(id) {
+  handleModal = (id) => {
     this.personModal = this.model.getPersonForModal(id);
     this.view.renderModal(this.personModal);
     this.view.modalDisplay();
     this.view.closeModalButton();
     this.view.closeModalOutside();
-  }
+  };
 
-  handleChangeoverNext(id) {
+  handleChangeoverNext = (id) => {
     this.personChangeoverNext = this.model.getPersonForChangeOver(id) + 1;
     this.view.renderModal(this.model.persons[this.personChangeoverNext]);
-  }
+  };
 
-  handleChangeoverPrev(id) {
+  handleChangeoverPrev = (id) => {
     this.personChangeoverPrev = this.model.getPersonForChangeOver(id) - 1;
     this.view.renderModal(this.model.persons[this.personChangeoverPrev]);
-  }
+  };
 }
 
 export { Controller };
